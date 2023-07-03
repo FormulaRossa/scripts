f8679b5fdd01ab10abd4e1feac4539b885117723
Lean Schuler <info@leanschuler.ch>
2023-07-03 10:32:59 +0200

    update messages

--- index.sh
+++ index.sh
@@ -1,32 +1,47 @@
+function warning() {
+    echo -e "\e[33m$1 \e[0m"
+}
+
+function success() {
+    echo -e "\e[32m$1 \e[0m"
+}
+
+function error() {
+    echo -e "\e[31m$1 \e[0m"
+}
+
+
 apt-get update >/dev/null
-echo "Success! Apt-get updated."
+success "Success! apt-get update done."
+
+apt-get upgrade -y >/dev/null
+success "Success! apt-get upgrade done."
 
 apt-get install -y git curl wget unzip python3-pip python3 >/dev/null
-echo "Success! Important packages installed."
+success "Success! apt-get install done."
 
 if [ -f /etc/motd ]; then
-    echo "MOTD already exists."
+    warning "MOTD already set up."
 else
     apt-get install -y figlet lolcat >/dev/null
     figlet -w 150 "$(sed 's/.*/\u&/' </etc/hostname)" | /usr/games/lolcat -f >/etc/motd
     chmod -x /etc/update-motd.d/* >/dev/null
-    echo "Success! MOTD set up."
+    success "Success! MOTD set up."
 fi
 
 if [ -x "$(command -v docker)" ]; then
-    echo "Docker already installed."
+    warning "Docker already installed."
 else
     curl -fsSL https://get.docker.com | sh >/dev/null
-    echo "Success! Docker installed."
+    success "Success! Docker installed."
 fi
 
 if [ -x "$(command -v docker-compose)" ]; then
-    echo "Docker Compose already installed."
+    warning "Docker Compose already installed."
 else
     sudo curl -L "https://github.com/docker/compose/releases/download/v2.19.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose >/dev/null
     chmod +x /usr/local/bin/docker-compose >/dev/null
-    echo "Success! Docker Compose installed."
+    warning "Success! Docker Compose installed."
 fi
 
-echo "----------------------------------------"
-echo -e "\e[32mSuccess! All done. \e[0m"
+success "Success! All done."
